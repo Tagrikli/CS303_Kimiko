8b3d770f14261ecdfe425532e6709e77467bee67
cankucukyilmaz <cankucukyilmaz@sabanciuniv.edu>
2021-12-03 16:02:32 +0300

    added semi-finished sendMessageForm

--- ReactUI_new/src/App.tsx
+++ ReactUI_new/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import Header from './Components/header';
 import MessageList, { DUMMY_DATA } from './Components/messageList';
+import sendMessageForm from './Components/sendMessageForm';
 
 class App extends React.Component<{},{messages: any[]}> {
 
@@ -18,6 +19,7 @@ class App extends React.Component<{},{messages: any[]}> {
       <div className="App">
         <Header/>
         <MessageList messages={this.state.messages}/>
+        <sendMessageForm sendMessage={this.sendMessage} />
       </div>
 
     )
