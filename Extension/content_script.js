3047d1d43a12e97f3be29ad01c070df8700613ce
Tugrul Agrikli <tagrikli@gmail.com>
2021-12-12 23:19:38 +0300

    toogle display

--- Extension/content_script.js
+++ Extension/content_script.js
@@ -1,7 +1,37 @@
-var div = document.createElement("iframe");
+let div = document.createElement("iframe");
 
 div.id = "kimiko-container";
 
 document.body.appendChild(div);
 div.src = "http://localhost:3000";
+
+const PANEL_WIDTH = 300;
+let PANEL_LEFT = 0;
+
+function toggleDisplay() {
+
+    if (PANEL_LEFT === 0) {
+        PANEL_LEFT = -PANEL_WIDTH;
+    } else {
+        PANEL_LEFT = 0;
+    }
+
+    div.style.left = `${PANEL_LEFT}px`;
+
+}
+
+div.style.width = `${PANEL_WIDTH}px`;
+div.style.left = '0px';
+
+
+
+document.addEventListener('keypress', (e) => {
+
+    if (e.shiftKey && e.code === 'KeyD') {
+        toggleDisplay();
+    }
+
+    console.log(e);
+
+})
 
