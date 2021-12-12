394dc0a990def3145e377c6093c5aa0bb0274809
Tugrul Agrikli <tagrikli@gmail.com>
2021-12-12 23:46:12 +0300

    toggle display

--- Extension/content_script.js
+++ Extension/content_script.js
@@ -1,14 +1,21 @@
-let div = document.createElement("iframe");
+let div = $('<iframe></iframe>');
 
-div.id = "kimiko-container";
+div.prop('id', "kimiko-container");
+div.prop('src', 'http://localhost:3000');
 
-document.body.appendChild(div);
-div.src = "http://localhost:3000";
+$(document.body).append(div);
+
+$('body, #kimiko-container').on('keypress', (e) => {
+    if (e.shiftKey && e.code === 'KeyD') {
+        toggleDisplay();
+    }
+})
+
 
 const PANEL_WIDTH = 300;
 let PANEL_LEFT = 0;
 
-function toggleDisplay() {
+function toggleDisplay(event) {
 
     if (PANEL_LEFT === 0) {
         PANEL_LEFT = -PANEL_WIDTH;
@@ -16,22 +23,10 @@ function toggleDisplay() {
         PANEL_LEFT = 0;
     }
 
-    div.style.left = `${PANEL_LEFT}px`;
-
+    div.css('left', `${PANEL_LEFT}px`);
 }
 
-div.style.width = `${PANEL_WIDTH}px`;
-div.style.left = '0px';
+div.css('width', `${PANEL_WIDTH}px`);
+div.css('left', '0px');
 
-
-
-document.addEventListener('keypress', (e) => {
-
-    if (e.shiftKey && e.code === 'KeyD') {
-        toggleDisplay();
-    }
-
-    console.log(e);
-
-})
 
